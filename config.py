0c77992e46f60513de49bf4722d9d3227c7ecaa9
AnHuynhIR <119585817+Anhuynh2404@users.noreply.github.com>
2025-02-14 07:09:36 +0000

    update method config_chapter1

--- config.py
+++ config.py
@@ -62,3 +62,60 @@ IS_LOCAL = (not IS_BINDER) and (not IS_COLAB)
 
 def download_to_colab(chapter, branch='main'):    
     base_url = 'https://raw.githubusercontent.com/anhuynh2404/Deeplearning_Stepbystep-Study/{}/'.format(branch)
+
+    folders = FOLDERS[chapter]
+    filenames = FILENAMES[chapter]
+    for folder, filename in zip(folders, filenames):
+        if len(folder):
+            try:
+                os.mkdir(folder)
+            except OSError as e:
+                if e.errno != errno.EEXIST:
+                    raise
+
+        if len(filename):
+            path = os.path.join(folder, filename)
+            url = '{}{}'.format(base_url, path)
+            r = requests.get(url, allow_redirects=True)
+            open(path, 'wb').write(r.content)
+
+    try:
+        os.mkdir('runs')
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+
+TB_LINK = ''
+if IS_BINDER:
+    TB_LINK = HTML('''
+    <a href="" target="_blank" id="tb">Click here to open TensorBoard</a>
+    <script>
+        var address=document.location.href;
+        a = document.getElementById('tb');
+        a.href = address.substr(0, address.lastIndexOf("/")-9).concat("proxy/6006/");
+    </script>
+    ''')
+
+def config_chapter0(branch='main'):
+    if IS_COLAB:
+        print('Downloading files from GitHub repo to Colab...')
+        download_to_colab(0, branch)
+        print('Finished!')
+
+def config_chapter1(branch='main'):
+    if IS_COLAB:
+        print('Installing torchviz...')
+        subprocess.run([sys.executable, '-m', 'pip', 'install', 'torchviz'])
+        print('Downloading files from GitHub repo to Colab...')
+        download_to_colab(1, branmainch)
+        print('Creating folders...')
+        folders = ['data_preparation', 'model_configuration', 'model_training']
+
+        for folder in folders:
+            try:
+                os.mkdir(folder)
+            except OSError as e:
+                e.errno
+                if e.errno != errno.EEXIST:
+                    raise
+        print('Finished!')
